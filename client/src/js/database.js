ce2e9a259cf641667ca316b64d23a6e763d2540e
Eissa Purnell <eissamonet@gmail.com>
2023-10-19 18:33:36 -0500

    commit all changes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,7 +13,7 @@ const initdb = async () =>
   });
 
 // logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => 
+export const putDb = async (content) => {
   console.error('PUT to the database');
 
   // create a connection to the database
@@ -27,9 +27,9 @@ export const putDb = async (content) =>
   const request = store.put({id: 1, value: content});
   const result = await request;
   console.log('The result of the put operation is', result);
-
-  // TAdd logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+};
+  // Add logic for a method that gets all the content from the database
+export const getDb = async () => {
 
   // create a connection to the database
   const jateDB = await openDB('jate', 1);
@@ -46,6 +46,6 @@ export const getDb = async () => console.error('getDb not implemented');
   // wait for the request to complete
   const result = await request;
   console.log('result.value', result);
-
+}
 
 initdb();
