dda797505196aba26ccb7e4774c7edc8e3d5cc20
Eissa Purnell <eissamonet@gmail.com>
2023-10-18 19:05:05 -0500

    added logic and content to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,39 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => 
+  console.error('PUT to the database');
 
-// TODO: Add logic for a method that gets all the content from the database
+  // create a connection to the database
+  const jateDB = await openDB('jate', 1);
+
+  // create a new transaction to the database
+  const tx = jateDB.transaction('jate', 'readwrite');
+
+  // object store
+  const store = tx.objectStore('jate');
+  const request = store.put({id: 1, value: content});
+  const result = await request;
+  console.log('The result of the put operation is', result);
+
+  // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
+
+  // create a connection to the database
+  const jateDB = await openDB('jate', 1);
+
+  // create a new transaction to the db
+  const tx = jateDB.transaction('jate', 'readonly');
+
+  // open object store
+  const store = tx.objectStore('jate');
+
+  // get all the content from the database
+  const request = store.getAll();
+
+  // wait for the request to complete
+  const result = await request;
+  console.log('result.value', result);
+
 
 initdb();
