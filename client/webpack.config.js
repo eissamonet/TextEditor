f3ecec59e7d446260cef24da72e32975555103f4
Eissa Purnell <eissamonet@gmail.com>
2023-10-19 15:55:19 -0500

    webpack config updated

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,8 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+// Add and configure workbox plugins for a service worker and manifest file.
+// Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -12,12 +12,10 @@ module.exports = () => {
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js',
-      database: './src/js/database.js',
-      editor: './src/js/editor.js',
-      header: './src/js/header.js',
+    
     },
     output: {
-      filename: '[name].bundle.js',
+      filename: 'main.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
